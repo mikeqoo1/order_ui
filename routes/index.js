8cf7c905f212412a562001cd62176c99bd23ad9f
seaflower <mikeqoo123@gmail.com>
2022-11-11 16:20:03 +0800

    更新回報

--- routes/index.js
+++ routes/index.js
@@ -44,48 +44,57 @@ router.get("/report", function (req, res, next) {
     // report_record.push("8450000000CH000100000098261259996  000030000000290000000050000B01111102085043763 230R                                                            0000050000      ")
     // report_record.push("8450000000DH000100000098261259996  000029000000000000000050000B01111102085046564 230R                                                            0000050000      ")
     var data
-    report_record.forEach(function (item, index, array) {
-        console.log(item, index);
-        公司別 = item.substr(0, 4)
-        傳送序號 = item.substr(4, 6)
-        異動別 = item.substr(10, 1)
-        櫃號 = item.substr(11, 1)
-        委託書號 = item.substr(12, 4)
-        網路單號 = item.substr(16, 6)
-        客戶帳號 = item.substr(22, 6)
-        檢查碼 = item.substr(28, 1)
-        商品代號 = item.substr(29, 6)
-        交易別 = item.substr(35, 1)
-        改前數量 = item.substr(36, 6)
-        改後數量 = item.substr(44, 6)
-        委託單價 = item.substr(52, 10)
-        買賣別 = item.substr(62, 1)
-        委託日期 = item.substr(63, 8)
-        委託時間 = item.substr(71, 9)
-        來源別 = item.substr(80, 1)
-        營業員 = item.substr(81, 3)
-        回報代碼 = item.substr(84, 1)
-        回報訊息 = item.substr(85, 60)
-        改價前單價 = item.substr(145, 10)
-        if (異動別 == "I")
-            console.log("新單回報")
-        if (異動別 == "C")
-            console.log("改量回報")
-        if (異動別 == "P")
-            console.log("改價回報")
-        if (異動別 == "D")
-            console.log("刪單回報")
-
-        console.log(公司別 + "|" + 傳送序號 + "|" + 異動別 + "|" + 櫃號 + "|" + 委託書號 + "|" + 網路單號 + "|" +
-            客戶帳號 + "|" + 檢查碼 + "|" + 商品代號 + "|" + 交易別 + "|" + 改前數量 + "|" + 改後數量 + "|" +
-            委託單價 + "|" + 買賣別 + "|" + 委託日期 + "|" + 委託時間 + "|" + 來源別 + "|" + 營業員 + "|" +
-            回報代碼 + "|" + 回報訊息 + "|" + 改價前單價)
+    console.log(report_record)
+    if (report_record.length == 0) {
         data = {
-            data: report_record,
-            筆數: report_record.length
+            data: "還沒有回報",
+            筆數: 0
         }
+        res.send(data);
+    } else {
+        report_record.forEach(function (item, index, array) {
+            console.log(item, index);
+            公司別 = item.substr(0, 4)
+            傳送序號 = item.substr(4, 6)
+            異動別 = item.substr(10, 1)
+            櫃號 = item.substr(11, 1)
+            委託書號 = item.substr(12, 4)
+            網路單號 = item.substr(16, 6)
+            客戶帳號 = item.substr(22, 6)
+            檢查碼 = item.substr(28, 1)
+            商品代號 = item.substr(29, 6)
+            交易別 = item.substr(35, 1)
+            改前數量 = item.substr(36, 6)
+            改後數量 = item.substr(44, 6)
+            委託單價 = item.substr(52, 10)
+            買賣別 = item.substr(62, 1)
+            委託日期 = item.substr(63, 8)
+            委託時間 = item.substr(71, 9)
+            來源別 = item.substr(80, 1)
+            營業員 = item.substr(81, 3)
+            回報代碼 = item.substr(84, 1)
+            回報訊息 = item.substr(85, 60)
+            改價前單價 = item.substr(145, 10)
+            if (異動別 == "I")
+                console.log("新單回報")
+            if (異動別 == "C")
+                console.log("改量回報")
+            if (異動別 == "P")
+                console.log("改價回報")
+            if (異動別 == "D")
+                console.log("刪單回報")
 
-    });
+            console.log(公司別 + "|" + 傳送序號 + "|" + 異動別 + "|" + 櫃號 + "|" + 委託書號 + "|" + 網路單號 + "|" +
+                客戶帳號 + "|" + 檢查碼 + "|" + 商品代號 + "|" + 交易別 + "|" + 改前數量 + "|" + 改後數量 + "|" +
+                委託單價 + "|" + 買賣別 + "|" + 委託日期 + "|" + 委託時間 + "|" + 來源別 + "|" + 營業員 + "|" +
+                回報代碼 + "|" + 回報訊息 + "|" + 改價前單價)
+            data = {
+                data: report_record,
+                筆數: report_record.length
+            }
+
+        });
+    }
     res.json(data);
 });
 
@@ -157,7 +166,11 @@ router.post("/OrderMsg", function (req, res, next) {
         //結束client端 連線
         SendClient.end();
         RecvCleint.end();
-        report_record.push(buffer);
+        var abc = buffer.toString().split("\n");
+        console.log(abc)
+        for (i = 0; i < abc.length; i++) {
+            report_record.push(abc[i]);
+        }
     })
 
     SendClient.on("error", function (err) {
